a0c65937c0d7f81cbab3839566d2883588b49a12
HasnainRaza026 <hasnainraza0026@gmail.com>
2025-08-03 07:01:05 +0500

    feat: add icon

--- manifest.config.ts
+++ manifest.config.ts
@@ -4,6 +4,12 @@ export default defineManifest({
   manifest_version: 3,
   name: 'Weather Extension',
   version: '1.0.0',
+  description: 'A simple weather extension using React and Vite',
+  icons: {
+    '16': 'icon.png',
+    '48': 'icon.png',
+    '128': 'icon.png',
+  },
   action: {
     default_popup: 'src/popup/index.html',
   },
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>Vite + React</h1>;
+  return (
+    <div className='w-3xs p-4'>
+      <h1 className='text-lg font-bold'>
+        Welcome to the Weather Extension Popup!
+      </h1>
+      <p>
+        This is a simple React application running in a browser extension popup.
+      </p>
+    </div>
+  );
 }
 
 export default App;
